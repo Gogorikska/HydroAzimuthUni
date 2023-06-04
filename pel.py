added third antenna, attempt at fixing azimuth calculation problem, setup for classification

pel.py
@@ -2,10 +2,12 @@ import tkinter as tk
 import numpy as np
 import turtle as t
 import scipy as sp
+import math as m
 from tkinter import messagebox
 
 root=tk.Tk()
 root.geometry("720x1280")
+
 
 class Setup():
     def _init_(self):
@@ -13,6 +15,8 @@ class Setup():
         self.phf=phf
         self.alf=alf
         self.tyf=tyf
+        self.F=F
+        self.t=t
         
     def plot(self,x,y,x0,y0):
         
@@ -22,8 +26,18 @@ class Setup():
         sys2.penup()
         sys2.setpos(x[1],y[1])
 
+    def CLSFK(self,F,t):
+        if F>=10000 and F<=50000 and t<=300:
+            print('torpedo')
+        if F>=3000 and F<=120000 and t>300:
+            print('ship')
+        if F>=800 and F<=16000 and t>=100:
+            print('submarine')
+        else:
+            print('miscelaneous')
+
     def ping(self):
-        M=2
+        M=3
         n=1024
         Fs=48000
         dt=1/Fs
@@ -49,10 +63,11 @@ class Setup():
         ale.configure(text=alf)
 
         ##координаты приемников
-        x=np.array([0,0])
-        y=np.array([-0.5,0.5])
+        x=d*np.cos(np.array([0,(np.pi/2), np.pi]))
+        y=d*np.sin(np.array([0,(np.pi/2), np.pi]))
 
-        Y=np.zeros([M,n])
+        Y=np.zeros([M,n],dtype = 'complex_')
+        Yn=np.zeros([M,n],dtype = 'complex_')
         tau=np.zeros(M)
         ntau=np.zeros(M)
         for i in range(0,M):
@@ -62,25 +77,25 @@ class Setup():
             ntau[i]=np.ceil(abs(tau[i])*Fs)
             
         for i in range(0,M):
-            utau=n+np.sum(ntau)
+            utau=n+np.sum(ntau[i])
             u0=np.random.randn(1,utau.astype(int))
             t0a=u0.size
             t0=np.array(np.arange(1,t0a+1))*dt+np.min(tau)
             ta=n+1
             t11=np.array(np.arange(1,ta))*dt
-            t1=t11-tau[i]
+            t1=t11+tau[i]
             u00=np.ndarray.flatten(u0)
-            S=np.interp(t1, t0, u00)
+            Sn=sp.interpolate.interp1d(t0, u00, kind='cubic', fill_value='extrapolate')
+            S=Sn(t1)
             Y[i,]=np.fft.fft(S,n)
+            Yn[i,]=np.conjugate(Y[i,])
 
-        Yc=np.conjugate(Y[1,])
-        Ym=Y[0,]*Yc
+        Ym=np.zeros([M,n],dtype = 'complex_')
+        Ym[0,]=Y[1,]*Yn[0,]
+        Ym[1,]=Y[2,]*Yn[0,]
+        Ym[2,]=Y[1,]*Yn[2,]
         Ypi=abs(np.fft.ifft(Ym,n))
         Yp=np.fft.ifftshift(Ypi)
-        Yi=abs(Yp)
-        Pi=max(Yi)
-        Yi1=Yi.tolist()
-        Pg=Yi1.index(Pi)+1
 
         Tc1=-(n-1)/2
         Tc2=(n-1)/2
@@ -90,8 +105,20 @@ class Setup():
         alsr=abs(np.emath.arcsin(ar))
         als=np.rad2deg(alsr)
         alsl=als.tolist()
-        ping=round(alsl[Pg])
-        phe.configure(text=ping)
+
+        kal=np.zeros(M)
+        for i in range(0,M):
+            Ye=Yp[i,]
+            Yen=Ye.tolist()
+            kln=Yen.index(max(Yen))
+            kal[i]=alsl[kln]
+
+        Yc=(kal[0]+kal[1])/2*np.cos(np.pi/6)
+        Ys=(1/3)*(kal[1]-kal[0]-2*kal[2])
+        Yt=m.atan2(Yc,Ys)*(180/np.pi)
+        Yi=(Yt-180)%360
+        
+        phe.configure(text=Yt)
         self.plot(x,y,x0,y0)
         
     def start(self):